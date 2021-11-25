af804122534d7ce0f5e474cae8f3d367449eff1c
Zaiste <oh@zaiste.net>
2021-11-25 16:22:04 +0100

    add the newsletter integration

---
+++ pages/api/subscribe.js
@@ -0,0 +1,38 @@
+// @ts-ignore
+export default async function subscribe(req, res) {
+  const { email } = req.body;
+
+  if (!email) {
+    return res.status(400).json({ error: 'Email jest wymagany' });
+  }
+
+  try {
+    const APIKEY = process.env.MAILERLITE_APIKEY || "";
+    const GROUP = process.env.MAILERLITE_GROUP || "";
+
+    const data = { email };
+
+    // 6. Send a POST request to Mailchimp.
+    const response = await fetch(
+      `https://api.mailerlite.com/api/v2/groups/${GROUP}/subscribers`,
+      {
+        body: JSON.stringify(data),
+        headers: {
+          'X-MailerLite-ApiKey': `${APIKEY}`,
+          'Content-Type': 'application/json'
+        },
+        method: 'POST'
+      }
+    );
+
+    if (response.status >= 400) {
+      return res.status(400).json({
+        error: `Pojawił się problem przy zapisie do Newslettera. Napisz do nas bezpośrednio na siema@zaisteprogramuj.pl`
+      });
+    }
+
+    return res.status(201).json({ error: '' });
+  } catch (error) {
+    return res.status(500).json({ error: error.message || error.toString() });
+  }
+};
