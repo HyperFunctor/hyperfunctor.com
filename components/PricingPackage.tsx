17dc73ad7fc414924b617a9bfa75ddf171e96012
Zaiste <oh@zaiste.net>
2021-12-16 18:21:38 +0100

    add pricing section

--- components/PricingPackage.tsx
+++ components/PricingPackage.tsx
@@ -7,33 +7,22 @@ export const PricingPackage = ({ className, pkg }) => {
     // eslint-disable-next-line @typescript-eslint/restrict-template-expressions -- @todo
     <div className={`overflow-hidden mt-12 lg:mt-0 ${className}`}>
       <div className="px-6 py-8 bg-white sm:p-10 sm:pb-6">
-        <div>
-          <span className="inline-flex px-4 py-1 rounded-full text-sm leading-5 font-semibold tracking-wide uppercase bg-blue-100 text-blue-600">
+        <div className="flex justify-center">
+          <span className="inline-flex px-4 py-1 rounded-full text-sm leading-5 font-semibold tracking-wide uppercase bg-pink-100 text-pink-600">
             {pkg.name}
           </span>
         </div>
-        <div className="mt-4 flex items-baseline text-6xl leading-none font-extrabold">
-          {pkg.price.discounted} zł
-          <span className="ml-4 text-3xl leading-8 font-medium text-gray-500 line-through">
-            {" "}
-            {pkg.price.regular} zł{" "}
-          </span>
-        </div>
-        <div className="text-center text-gray-500 mt-2">
-          {pkg.cheaper}{" "}
-          <span className="font-bold text-blue-500">
-            {pkg.price.regular - pkg.price.discounted} zł
-          </span>{" "}
-          {pkg.until}
+        <div className="mt-4 flex justify-center items-baseline text-6xl leading-none font-extrabold">
+          {pkg.price} zł
         </div>
       </div>
-      <div className="px-6 pt-6 pb-8 bg-gray-50 sm:p-10 sm:pt-6">
+      <div className="px-6 pt-6 pb-8 bg-gray-50 border-t sm:p-10 sm:pt-6">
         <ul>
           {pkg.elements.map((element, idx) => (
             <li className="flex items-start mb-4" key={idx}>
               <div className="flex-shrink-0">
                 <svg
-                  className="h-6 w-6 text-green-500"
+                  className="h-6 w-6 text-pink-500"
                   stroke="currentColor"
                   fill="none"
                   viewBox="0 0 24 24"
@@ -56,7 +45,7 @@ export const PricingPackage = ({ className, pkg }) => {
         <div className="mt-6 rounded-md shadow">
           {/* eslint-disable-next-line @typescript-eslint/restrict-template-expressions -- @todo */}
           <Link href={`/order/${pkg.key}`}>
-            <a className="w-full flex items-center justify-center px-5 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-blue-400 hover:bg-blue-500 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out">
+            <a className="w-full flex items-center justify-center px-5 py-3 border border-transparent text-lg leading-6 font-bold rounded-md text-white bg-pink-400 hover:bg-pink-500 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out">
               {pkg.buy}
             </a>
           </Link>
