763aa23828129227244106ec642c07639eb10ba5
Michal Miszczyszyn <michal@mmiszy.pl>
2023-02-04 15:47:00 +0100

    testimonials

--- components/next/Testimonials.tsx
+++ components/next/Testimonials.tsx
@@ -1,14 +1,58 @@
 import Image from "next/image";
 
+import Marcin from "/images/testimonials/marcin-parda.jpg";
 import Larto from "/images/testimonials/konrad-nojman.png";
 import Siemion from "/images/testimonials/jakub-semienowicz.jpg";
 import Pawel from "/images/testimonials/pawel-pikus.webp";
 
 const testimonials = [
   {
+    photo: Marcin,
+    name: "Marcin Parda",
+    role: "JavaScript Developer w STX Next",
+    className: "col-span-2",
+    text: (
+      <>
+        <p>
+          Kupiłem kurs <strong>jako mid JS developer</strong>, chcąc nauczyć się
+          Next.js i GraphQL. Z materiałów wyłuskałem{" "}
+          <strong>
+            masę dobrych praktyk, ciekawych porad, optymalizacji tworzenia kodu
+            i przydatnych Reactowych wzorców
+          </strong>
+          . Treść Zadań nie narzuca sposobu rozwiązania, więc trzeba się wykazać{" "}
+          <strong>kreatywnością</strong>, pogrzebać w dokumentacji i popytać
+          innych osób. Pozostali kursanci oferowali nie tylko swoją pomoc, ale
+          też wspólną naukę przy jednym projekcie! Dobrze się to zgrywa z
+          możliwością{" "}
+          <strong>
+            zadawania pytań na discordzie i podczas konsultacji live
+          </strong>{" "}
+          z mentorami.
+        </p>
+        <p>
+          <strong>Kuba i Michał mają świetną chemię</strong> na ekranie, przez
+          co wprowadzają luźniejszą atmosferę podczas lekcji i konsultacji.
+          Dzięki temu łatwiej było przełamać strach przed zadaniem pytania
+          i włączeniem kamerki na spotkaniach.
+        </p>
+        <p>
+          Kurs polecam{" "}
+          <strong>
+            osobom chcącym zostać frontendowcami, juniorom oraz midom
+          </strong>
+          , którzy chcieliby mieć większą styczność z Next.js. Swoją drogą sam{" "}
+          <strong>kurs zdążył się zwrócić</strong>, bo dzięki wiedzy nabytej w
+          nim udało mi się “błysnąć” na rozmowie o podwyżkę, czego rezultatem
+          było podbicie seniority. Dzięki, chłopaki.
+        </p>
+      </>
+    ),
+  },
+  {
     photo: Larto,
     name: "Konrad Nojman",
-    role: "Senior JS Developer",
+    role: "JavaScript Developer w HTD Health",
     text: (
       <p>
         W kursie podoba mi się wyjątkowo organiczne podejście do uczestników
@@ -24,7 +68,7 @@ const testimonials = [
   {
     photo: Siemion,
     name: "Jakub Siemienowicz ",
-    role: "Junior Frontend Developer w Grey Group Poland",
+    role: "React Developer w Today",
     text: (
       <p>
         Kurs prowadzony w konwencji <em>pair programmingu</em> był strzałem
@@ -34,7 +78,7 @@ const testimonials = [
           deweloperzy
         </strong>
         . Jako tutorialowy wyjadacz, mogę Ci zagwarantować, że materiały na tym
-        kursie są zupełnie innej jakości niż to, co znajdziesz na youtube czy
+        kursie są zupełnie innej jakości niż to, co znajdziesz na YouTube czy
         Udemy.
       </p>
     ),
@@ -42,7 +86,8 @@ const testimonials = [
   {
     photo: Pawel,
     name: "Paweł Pikus",
-    role: "JavaScript Developer",
+    role: "Test Automation Engineer w Huuuge Games",
+    className: "col-span-2",
     text: (
       <>
         <p>
@@ -80,27 +125,25 @@ const testimonials = [
 export function Testimonials() {
   return (
     <section className="bg-white py-16" id="opinie">
-      <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-5 lg:items-start">
-        <div className="mx-4 mb-4 sm:mx-auto max-w-md sm:max-w-3xl sm:px-6 lg:px-0 lg:col-span-2 mr-8 self-center">
-          <div className="text-gray-500 prose lg:prose-xl prose-pink">
-            <h2 className="font-extrabold tracking-tight">
-              Co mówią nasi kursanci?
-            </h2>
-            <p className="lead">
-              Przeszkoliliśmy setki osób, a z naszych materiałów online
-              skorzystało <strong>pół miliona ludzi</strong>.
-            </p>
-            <p className="lead">Co o nas mówią?</p>
-          </div>
+      <div className="mx-4 mb-4 sm:px-6 lg:mx-auto lg:max-w-4xl">
+        <div className="text-gray-500 prose lg:prose-xl prose-pink">
+          <h2 className="font-extrabold tracking-tight">
+            Co mówią nasi kursanci?
+          </h2>
+          <p className="lead">
+            Przeszkoliliśmy setki osób, a z naszych materiałów online
+            skorzystało <strong>pół miliona ludzi</strong>.
+          </p>
+          <p className="lead">Co o nas mówią?</p>
         </div>
 
-        <ul className="text-base max-w-4xl mx-auto lg:max-w-none col-span-3 flex flex-row flex-wrap justify-center items-center">
+        <ul className="text-base lg:grid lg:grid-cols-2 gap-x-8 gap-y-12 mt-8">
           {testimonials.map((testimonial) => (
             <li
               key={testimonial.name}
-              className="group sm:w-1/2 mb-8 px-4 sm:px-0 sm:even:pl-4 sm:odd:pr-4 sm:last:odd:px-2 sm:last:odd:w-full"
+              className={`group ${testimonial.className || ""}`}
             >
-              <blockquote className="group-odd:bg-white group-odd:text-gray-700 group-even:text-white group-even:bg-pink-800 rounded-lg shadow-lg flex flex-col px-6 py-8 sm:px-6 sm:pt-8 sm:pb-6 relative before:content-['”'] before:font-serif before:text-9xl before:absolute before:top-0 before:right-2">
+              <blockquote className="group-odd:bg-white h-full group-odd:text-gray-700 group-even:text-white group-even:bg-pink-900 rounded-lg shadow-lg flex flex-col px-6 py-8 sm:px-6 sm:pt-8 sm:pb-6 relative before:content-['”'] before:font-serif before:text-9xl before:absolute before:top-0 before:right-2">
                 <div className="prose-p:mb-2 order-2 rounded-t-lg text-lg font-normal">
                   {testimonial.text}
                 </div>
